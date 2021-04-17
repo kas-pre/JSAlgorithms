96010fa8a59af7ac16f650daa72bb772fcbc2d23
Kasasira Precious <kasasirakp40@gmail.com>
2021-04-17 18:28:32 +0300

    general problem solving pattern

--- Algorithms/Algos&PS.js
+++ Algorithms/Algos&PS.js
@@ -68,6 +68,7 @@
 // "Hello hi"
 // charCount("") // {}, null, false, undefined
 // explore all other edge cases
+// Handling errors
 
 // Break it Down
 // Take the actual steps of the solution & write it down
@@ -120,3 +121,88 @@
 // 	return result;
 // }
 // console.log(charCount('hello'));
+
+// Look Back & Refactor
+// Congrats on solving it, but you are not done!
+
+// Refactoring questions
+// Can you check the result?
+// can you derive the result differently?
+// Can you understand it at a glance?
+// Can you use the result or method for some other problem?
+// Can you improve the performance of your solution?
+// Can you think of other ways to refactor?
+// How have other people solved this problem?
+
+// Ex. refactored 1
+// function charCount(str) {
+// 	var obj = {};
+// 	for (var i = 0; i < str.length; i++) {
+// 		var char = str[i].toLowerCase();
+// 		if (/[a-z0-9]/.test(char)) {
+// 			if (obj[char] > 0) {
+// 				obj[char]++;
+// 			} else {
+// 				obj[char] = 1;
+// 			}
+// 		}
+// 	}
+// 	return obj;
+// }
+// console.log(charCount("Hi JS Code 2!"));
+
+// Ex. refactored 2
+// function charCount(str) {
+// 	var obj = {};
+// 	for (var char of str) {
+// 		char = char.toLowerCase();
+// 		if (/[a-z0-9]/.test(char)) {
+// 			if (obj[char] > 0) {
+// 				obj[char]++;
+// 			} else {
+// 				obj[char] = 1;
+// 			}
+// 		}
+// 	}
+// 	return obj;
+// }
+// console.log(charCount("Hi JS Code 2!"));
+
+// Ex. refactored 3
+// function charCount(str) {
+// 	var obj = {};
+// 	for (var char of str) {
+// 		char = char.toLowerCase();
+// 		if (/[a-z0-9]/.test(char)) {
+// 			obj[char] = ++obj[char] || 1;
+// 		}
+// 	}
+// 	return obj;
+// }
+// console.log(charCount("Hi JS Code 2!"));
+
+// Ex. refactored 4
+// function charCount(str) {
+// 	let obj = {};
+// 	for (let char of str) {
+// 		if (isAlphaNumeric(char)) {
+//             char = char.toLowerCase();
+// 			obj[char] = ++obj[char] || 1;
+// 		}
+// 	}
+// 	return obj;
+// }
+// console.log(charCount('Hi JS Code 2!'));
+
+// function isAlphaNumeric(char) {
+// 	var code = char.charCodeAt(0);
+// 	if (
+// 		!(code > 47 && code < 58) && //numeric (0-9)
+// 		!(code > 64 && code < 91) && //upper alpha (A-Z)
+// 		!(code > 96 && code < 123) //lower alpha (a-z)
+// 	) {
+// 		return false;
+// 	}
+// 	return true;
+// }
+
